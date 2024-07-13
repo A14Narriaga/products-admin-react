2ede67a0712545f493b82e8e8e481bfbaba85295
Alan Arriaga <A14N.arriaga@gmail.com>
2024-07-12 20:33:07 -0600

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import "./main.scss"
+
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+import App from "./app.tsx"
+
+ReactDOM.createRoot(document.querySelector("#root")!).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+)
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import path from "node:path"
+
+import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from "vite"
+
+// https://vitejs.dev/config/
+
+const port = Number(process.env.PORT) || 4000
+
+export default defineConfig({
+	base: "./",
+	plugins: [react()],
+  resolve: {
+		alias: {
+			"@src": path.resolve(__dirname, "./src"),
+			"@tests": path.resolve(__dirname, "./tests")
+		}
+	},
+	server: {
+		port,
+		strictPort: true,
+		host: true,
+		origin: `http://0.0.0.0:${port}`
+	}
+})
